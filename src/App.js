454943a65d295b4a66e9b873673864a4c6e46808
sammy-code98 <sammysmart98@yahoo.com>
2022-08-19 19:48:00 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import './App.css';
+import "./App.css";
 
 function App() {
+  // helper function to modify Filereader callbacks into async/await
+  function modifyFileAsync(file) {
+    return new Promise((resolve, reject) => {
+      let reader = new FileReader();
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+      reader.onerror = reject;
+      reader.readAsArrayBuffer(file);
+    });
+  }
+
+
+  const fileSelected = async (e) => {
+    const fileList = e.target.files
+    if(fileList?.length > 0){
+      const pdfArrayBuffer = await modifyFileAsync(fileList[0])
+    }
+    console.log("helloe biy");
+  };
   return (
     <div className="App">
-    hello
+      <div>
+        <input
+          type="file"
+          id="file-picker"
+          accept=".pdf"
+          onChange={fileSelected}
+        />
+      </div>
     </div>
   );
 }
