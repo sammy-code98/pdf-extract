127f4a21e2ddb8e51aeab0f93ad305006ab2d1c1
sammy-code98 <sammysmart98@yahoo.com>
2022-08-19 19:54:51 +0100

    add range function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { PDFDocument } from "pdf-lib";
 
 function App() {
   // helper function to modify Filereader callbacks into async/await
@@ -13,13 +14,23 @@ function App() {
     });
   }
 
+  // generate number of pages you want to extract
+
+  function pdfRange(start, end) {
+    let length = end - start + 1;
+
+    // Here we add -1 at the end. Do you know the reason? Yes – in programming,
+    // indexes start from 0, not 1. So we have to deduct -1
+    // from every page number to get the behaviour we want.
+    return Array.from({ length }, (_, i) => start + i - 1);
+  }
 
   const fileSelected = async (e) => {
-    const fileList = e.target.files
-    if(fileList?.length > 0){
-      const pdfArrayBuffer = await modifyFileAsync(fileList[0])
+    const fileList = e.target.files;
+    if (fileList?.length > 0) {
+      const pdfArrayBuffer = await modifyFileAsync(fileList[0]);
     }
-    console.log("helloe biy");
+    console.log(fileList);
   };
   return (
     <div className="App">
